5e5a0824a6d36441d50b5234d95e4d95037d3bd0
Iamneo <techadmin@iamneo.ai>
2023-09-22 08:01:29 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -11,4 +11,6 @@ provider "aws"{
     access_key ="AKIA2CDM5VS3J3HDGD57"
     secret_key ="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"
 }
-resource "aws_instance
+resource "aws_instance" "aws"{
+    ami = ""
+}
